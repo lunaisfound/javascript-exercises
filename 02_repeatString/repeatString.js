9a42420198dfe66a6b0538f1afad296175cfd091
lunaisfound <130017998+lunaisfound@users.noreply.github.com>
2025-03-26 02:56:03 -0400

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num > -1) {
+    let result = "";
+    while (num > 0) {
+      result += string;
+      num -= 1;
+    }
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
